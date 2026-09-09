3e0e9f9d4f2ef83281f6d174c4fb5141ccd0d48b
אביאל קדם <avielkedem2@gmail.com>
2026-09-09 15:33:41 +0300

    add a middleware for the client

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers.route import router
 
 
@@ -7,4 +8,13 @@ app = FastAPI()
 app.include_router(router)
 
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+      "http://localhost:5173",      
+    ],
+    allow_headers=["*"],
+    allow_methods=["*"],
+    allow_credentials=True
+)
 
--- backend/app/routers/route.py
+++ backend/app/routers/route.py
@@ -3,15 +3,17 @@ from services import open_meteo
 
 
 router = APIRouter()
-
+@router.get("/health")
+async def health():
+    return "The server working"
 
 @router.get("/search-city/:city")
-def get_city(city:str):
+async def get_city(city:str):
     cities = open_meteo.get_list_city(city)
     return open_meteo.get_details_of_city(cities)
 
 
 @router.get("/compares")
-def hello_world(latitude:float, longitude:float, latitude2:float, longitude2:float):
+async def hello_world(latitude:float, longitude:float, latitude2:float, longitude2:float):
     cities = [{"latitude": latitude, "longitude": longitude}, {"latitude": latitude2, "longitude": longitude2}]
     return open_meteo.get_details_of_city(cities)
